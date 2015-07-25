8c2ad9a0e6fefaa2fcfb84e2c8e10674ae7eaa31
Chad Wood <cdw@cs.uoregon.edu>
2015-07-24 19:06:13 -0700

    Beginning to hammer the sos into shape for sos_flow purposes.

--- src/sos.h
+++ src/sos.h
@@ -42,42 +42,43 @@
 
 
 enum SOS_role {
-  SOS_APP,
-  SOS_MONITOR,
-  SOS_DB,
-  SOS_POWSCHED,
-  SOS_SURPLUS,
-  SOS_ANALYTICS
+    SOS_CLIENT,
+    SOS_DAEMON,
+    SOS_DB
 };
 
 enum SOS_msg {
-  SOS_MSG_ANNOUNCE,
-  SOS_MSG_PUBLISH,
-  SOS_MSG_UNANNOUNCE,
-  SOS_MSG_LIST_PUBS,
-  SOS_MSG_SUBSCRIBE,
-  SOS_MSG_REFRESH,
-  SOS_MSG_UNSUBSCRIBE,
-  SOS_MSG_SHUTDOWN
+    SOS_MSG_ANNOUNCE,
+    SOS_MSG_REANNOUNCE,
+    SOS_MSG_PUBLISH,
+    SOS_MSG_POST,
+    SOS_MSG_UNANNOUNCE,
+    SOS_MSG_SUBSCRIBE,
+    SOS_MSG_UNSUBSCRIBE,
+    SOS_MSG_REFRESH,
+    SOS_MSG_SHUTDOWN
 };
 
 enum SOS_type {
-  SOS_INT     = 0,
-  SOS_LONG    = 1,
-  SOS_DOUBLE  = 2,
-  SOS_STRING  = 3
+    SOS_INT,
+    SOS_LONG,
+    SOS_DOUBLE,
+    SOS_STRING
 };
 
 enum SOS_aggregate {
-  SOS_SUM,
-  SOS_MIN,
-  SOS_MAX,
-  SOS_MEAN,
-  SOS_STDDEV
+    SOS_SUM,
+    SOS_MIN,
+    SOS_MAX,
+    SOS_MEAN,
+    SOS_STDDEV
 };
 
 enum SOS_scope {
-  SOS_SCOPE_GLOBAL
+    SOS_SCOPE_SELF,
+    SOS_SCOPE_NODE,
+    SOS_SCOPE_ENCLAVE,
+    SOS_SCOPE_GLOBAL
 };
 
 typedef enum SOS_role       SOS_role;
@@ -87,59 +88,59 @@ typedef enum SOS_aggregate  SOS_aggregate;
 typedef enum SOS_scope      SOS_scope;
 
 typedef union {
-  int           i_val;
-  long          l_val;
-  double        d_val;
-  char         *c_val;
+    int           i_val;
+    long          l_val;
+    double        d_val;
+    char         *c_val;
 } SOS_val;
 
 typedef struct {
-  long        guid;
-  SOS_type    type;
-  SOS_val     val;
-  char       *name;
-  char       *channel;
-  int         dirty;
-  double      pack_ts;
-  double      send_ts;
-  double      recv_ts;
-  int         semantic_hint;
-  char       *pragma_msg;
-  int         pragma_len;
-  int         update_priority;
-  int         src_layer;
-  int         src_role;
-  int         scope_hint;
-  int         retention_policy;
+    long        guid;
+    SOS_type    type;
+    SOS_val     val;
+    char       *name;
+    char       *channel;
+    int         dirty;
+    double      pack_ts;
+    double      send_ts;
+    double      recv_ts;
+    int         semantic_hint;
+    char       *pragma_msg;
+    int         pragma_len;
+    int         update_priority;
+    int         src_layer;
+    int         src_role;
+    int         scope_hint;
+    int         retention_policy;
 } SOS_data;
 
 typedef struct {
-  int         origin_pub_uid;
-  int         origin_node_id;
-  int         origin_comm_rank;
-  int         origin_process_id;
-  int         origin_thread_id;
-  SOS_role    origin_role;
-  char       *origin_prog_name;
-  char       *origin_prog_ver;
-  int         target_list;
-  char       *pragma_msg;
-  int         pragma_len;
-  char       *title;
-  int         announced;
-  int         elem_max;
-  int         elem_count;
-  SOS_data  **data;
+    int         origin_pub_uid;
+    int         origin_node_id;
+    int         origin_comm_rank;
+    int         origin_process_id;
+    int         origin_thread_id;
+    SOS_role    origin_role;
+    char       *origin_prog_name;
+    char       *origin_prog_ver;
+    int         target_list;
+    char       *pragma_msg;
+    int         pragma_len;
+    char       *title;
+    int         announced;
+    int         elem_max;
+    int         elem_count;
+    SOS_data  **data;
 } SOS_pub_handle;
 
 typedef struct {
-  int             suid;
-  int             active;
-  pthread_t       thread_handle;
-  int             refresh_delay;
-  SOS_role        source_role;
-  int             source_rank;
-  SOS_pub_handle *pub;
+    int             suid;
+    int             active;
+    pthread_t       thread_handle;
+    int             refresh_delay;
+    SOS_role        source_role;
+    int             source_rank;
+    SOS_pub_handle *pub;
 } SOS_sub_handle;
 
 
@@ -162,158 +163,209 @@ SOS_role           SOS_ROLE;
 
 
 
-char               SOS_pub_buffer[1000000];
-char               SOS_ann_buffer[1000000];
+char               SOS_pub_buffer[1024000];
+char               SOS_ann_buffer[1024000];
+char               SOS_pos_buffer[512000];
+
 
 /* Required if included by C++ code. */
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
+
 // ----------- [util] ----------------------
 
 
-  /*
-   * Function..: SOS_init
-   * Purpose...: Configure any preliminary values needed before other
-   *             parts of the SOS system can work.  This should be
-   *             called immediately after MPI_Init().
-   *
-   */
-  void SOS_init( int *argc, char ***argv, SOS_role role );
+    /*
+     * Function..: SOS_init
+     * Purpose...: Configure any preliminary values needed before other
+     *             parts of the SOS system can work.  This should be
+     *             called immediately after MPI_Init().
+     *
+     */
+    void SOS_init( int *argc, char ***argv, SOS_role role );
 
-  /*
-   * Function..: SOS_finalize
-   * Purpose...: Free up the MPI communicators created during the split.
-   *
-   */
-  void SOS_finalize();
+
+
+    /*
+     * Function..: SOS_finalize
+     * Purpose...: Free up the MPI communicators created during the split.
+     *
+     */
+    void SOS_finalize();
   
 
 
-  /*
-   * Function..: SOS_next_serial   [THREAD SAFE]
-   * Purpose...: Return a unique (per process) ID for use in keeping
-   *             track of replies to subscription requests, etc.
-   */
-  int SOS_next_serial();
-
-  /*
-   * Function..: SOS_strip_str
-   * Purpose...: Convenience function to clear out extended characters from a string.
-   *
-   */
-  void SOS_strip_str(char *str);
+    /*
+     * Function..: SOS_next_serial   [THREAD SAFE]
+     * Purpose...: Return a unique (per process) ID for use in keeping
+     *             track of replies to subscription requests, etc.
+     */
+    int SOS_next_serial();
 
 
-  /*
-   * Function..: SOS_new_pub
-   * Purpose...: Create a new publication handle to work with.
-   * Notes.....: By default, all publications target SOS_DB.
-   *
-   *             Additional targets can be added with SOS_add_target() calls.
-   */
-  SOS_pub_handle* SOS_new_pub(char *pub_name);
+    /*
+     * Function..: SOS_expand_data
+     * Purpose...: 
+     *
+     */
+    void SOS_expand_data( SOS_pub_handle *pub );
 
 
-  /*
-   * Function..: SOS_pack
-   * Purpose...: Helper function that packs name/val arrays into a handle.
-   * Notes.....: It manages all malloc/free internally, and grows pubs to support
-   *             arbitrarily large schemas as needed.  This SOS_pack() function
-   *             is how you define your schema to SOS.
-   *
-   *             The recommended behavior is to pack empty values into each
-   *             unique 'name' you intend to publish, and then announce, all
-   *             during your program initialization.  After that, you can
-   *             make calls to SOS_pack() OR SOS_repack() to update values, and
-   *             make calls to SOS_publish() to send all changed values out.
-   *
-   *             -- PERFORMANCE ADVISORY --
-   *
-   *             This function returns the index of the name in the pub's
-   *             data store.  If you capture that value, you can use it when
-   *             making calls to SOS_repack().  This can be considerably faster
-   *             depending on your schema's size and ordering.  SOS_repack has
-   *             O(1) performance with low overhead, vs. O(n) for SOS_pack
-   *             and string comparisons across a (potentially) linear scan of
-   *             every previously packed data element for a matching name.
-   */
-  int SOS_pack( SOS_pub_handle *pub, const char *name, SOS_type pack_type, SOS_val pack_val );
+    /*
+     * Function..: SOS_strip_str
+     * Purpose...: Convenience function to clear out extended characters from a string.
+     *
+     */
+    void SOS_strip_str(char *str);
 
 
-  /*
-   * Function..: SOS_repack
-   * Purpose...: 
-   *
-   */
-  void SOS_repack( SOS_pub_handle *pub, int index, SOS_val pack_val );
+
+    /*
+     * Function..: SOS_apply_announce / SOS_apply_publish
+     * Purpose...:
+     *
+     */
+    void SOS_apply_announce( SOS_pub_handle *pub, char *msg, int msg_len );
+    void SOS_apply_publish( SOS_pub_handle *pub, char *msg, int msg_len );
+
+
+
+    /*
+     * Function..: SOS_pack
+     * Purpose...: Helper function that packs name/val arrays into a handle.
+     * Notes.....: It manages all malloc/free internally, and grows pubs to support
+     *             arbitrarily large schemas as needed.  This SOS_pack() function
+     *             is how you define your schema to SOS.
+     *
+     *             The recommended behavior is to pack empty values into each
+     *             unique 'name' you intend to publish, and then announce, all
+     *             during your program initialization.  After that, you can
+     *             make calls to SOS_pack() OR SOS_repack() to update values, and
+     *             make calls to SOS_publish() to send all changed values out.
+     *
+     *             -- PERFORMANCE ADVISORY --
+     *
+     *             This function returns the index of the name in the pub's
+     *             data store.  If you capture that value, you can use it when
+     *             making calls to SOS_repack().  This can be considerably faster
+     *             depending on your schema's size and ordering.  SOS_repack has
+     *             O(1) performance with low overhead, vs. O(n) for SOS_pack
+     *             and string comparisons across a (potentially) linear scan of
+     *             every previously packed data element for a matching name.
+     */
+    int SOS_pack( SOS_pub_handle *pub, const char *name, SOS_type pack_type, SOS_val pack_val );
+
+
+
+    /*
+     * Function..: SOS_repack
+     * Purpose...: 
+     *
+     */
+    void SOS_repack( SOS_pub_handle *pub, int index, SOS_val pack_val );
+
+
+
+    /*
+     * Function..: SOS_get_val
+     * Purpose...: 
+     *
+     */
+    void SOS_get_val( SOS_pub_handle *pub, char *name );
+
+
+    
+
+    /*
+     * Function..: SOS_new_pub
+     * Purpose...: Create a new publication handle to work with.
+     *
+     */
+    SOS_pub_handle* SOS_new_pub(char *pub_name);
+
+
+    /*
+     * Function..: SOS_new_sub
+     * Purpose...: 
+     *
+     */
+    SOS_sub_handle* SOS_new_sub();
+  
+
+    /*
+     * Function..: SOS_free_pub / SOS_free_sub
+     * Purpose...:
+     *
+     */
+    void SOS_free_pub( SOS_pub_handle *pub );
+    void SOS_free_sub( SOS_sub_handle *sub );
+
+
+      /*
+      * Function..: SOS_display_pub
+      * Purpose...: 
+      *
+      */
+    void SOS_display_pub(SOS_pub_handle *pub, FILE *output_to);
+  
+  
+  
+    // ---------- [pub] ----------------------
+  
+  
+  
+  
+    /*
+     * Function..: SOS_announce
+     * Purpose...: 
+     */
+    void SOS_announce( SOS_pub_handle *pub );
+  
+  
+    /*
+     * Function..: SOS_publish()
+     * Purpose...: 
+     */
+    void SOS_publish( SOS_pub_handle *pub );
+
+
+  
+    /*
+     * Function..: SOS_unannounce
+     * Purpose...: 
+     */
+    void SOS_unannounce( SOS_pub_handle *pub );
+  
+  
+  
+  
+    // --------- [sub] --------------------
+
+
+    /*
+     *  TODO:{ SUBSCRIPTION, CHAD }
+     *
+     *  This section is a huge work-in-progress...
+     */
   
 
 
-  /*
-   * Function..: SOS_display_pub
-   * Purpose...: 
-   *
-   */
-  void SOS_display_pub(SOS_pub_handle *pub, FILE *output_to);
-  
-  
-  
-  // ---------- [pub] ----------------------
-  
-  
-  
-  
-  /*
-   * Function..: SOS_announce
-   * Purpose...: 
-   */
-  void SOS_announce( SOS_pub_handle *pub );
-  
-  
-  /*
-   * Function..: SOS_publish()
-   * Purpose...: 
-   */
-  void SOS_publish( SOS_pub_handle *pub );
-
-
-  
-  /*
-   * Function..: SOS_unannounce
-   * Purpose...: 
-   */
-  void SOS_unannounce( SOS_pub_handle *pub );
-  
-  
-  
-  
-  // --------- [sub] --------------------
-
-
-  /*
-   *  TODO:{ SUBSCRIPTION, CHAD }
-   *
-   *  This section is a huge work-in-progress...
-   */
-  
-
-
-  /* 
-   * Function..: SOS_subscribe
-   * Purpose...: 
-   */
-  SOS_sub_handle* SOS_subscribe( SOS_role target_role, int target_rank, char *pub_title, int refresh_ms );
+    /* 
+     * Function..: SOS_subscribe
+     * Purpose...: 
+     */
+    SOS_sub_handle* SOS_subscribe( SOS_role target_role, int target_rank, char *pub_title, int refresh_ms );
 
 
 
-  /*
-   * Function..: SOS_unsubscribe
-   * Purpose...: 
-   */
-  void SOS_unsubscribe( SOS_sub_handle *sub );
+    /*
+     * Function..: SOS_unsubscribe
+     * Purpose...: 
+     */
+    void SOS_unsubscribe( SOS_sub_handle *sub );
 
 
 
