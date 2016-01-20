6c544b0c50731f51cca13026a6cd50087948c017
Kevin Huck <khuck@cs.uoregon.edu>
2016-01-19 17:43:13 -0800

    Adding first worker example program.

---
+++ examples/synthetic_1/main.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+*   SOS_Flow example.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mpi.h>
+#include "worker.h"
+
+int main (int argc, char *argv[]) 
+{
+
+  int rc = MPI_SUCCESS;
+  int provided = 0;
+  rc = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  printf("MPI_Init_thread: provided = %d, MPI_THREAD_MULTIPLE=%d\n", provided, MPI_THREAD_MULTIPLE);
+  if (rc != MPI_SUCCESS) {
+    char *errorstring;
+    int length = 0;
+    MPI_Error_string(rc, errorstring, &length);
+    printf("Error: MPI_Init failed, rc = %d\n%s\n", rc, errorstring);
+    exit(1);
+  }
+
+  worker_a();
+
+  MPI_Finalize();
+  printf ("Done.\n");
+
+  return 0;
+}
+
---
+++ examples/synthetic_1/worker.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int worker_a(void);
