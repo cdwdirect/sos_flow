21055d90407c744ec6450cb3a484b4a969adc4c4
Kevin Huck <khuck@cs.uoregon.edu>
2016-04-21 16:16:33 -0700

    environment for edison

---
+++ sourceme-edison.sh
@@ -0,0 +1,21 @@
+module unload darshan
+module load cmake
+module load sqlite 
+module swap PrgEnv-intel PrgEnv-gnu
+
+export SOS_CMD_PORT=22500
+export SOS_DB_PORT=22503
+export SOS_ROOT=$HOME/src/sos_flow
+export SOS_WORK=.
+
+TAU=$HOME/src/tau2
+#ADIOS=$HOME/install/adios/1.9.0_gcc-4.9
+# ADIOS=$HOME/install/adios/1.9.0_gcc-4.9-tau
+# CHAOS=$HOME/install/chaos/stable
+# MXML=/usr/local/packages/mxml-2.7/gcc-4.5.3
+# PATH=$HOME/install/gdb/7.9/bin:$ADIOS/bin:$TAU/x86_64/bin:$SOS_ROOT/bin:$PATH
+# LD_LIBRARY_PATH=$CHAOS/lib:$ADIOS/lib:$MXML/lib:$LD_LIBRARY_PATH
+# CPATH=$ADIOS/include:$CPATH
+
+PATH=$TAU/craycnl/bin:$SQLITE_ROOT/bin:$PATH
+export TAU_MAKEFILE=${TAU}/craycnl/lib/Makefile.tau-gnu-mpi-pthread-pdt
