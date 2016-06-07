51bcabd7f94ea4709a6586ed78876880e4370a99
Chad Wood <cdw@cs.uoregon.edu>
2016-06-07 00:57:53 -0700

    Aciss script tweaks.

--- jobs/aciss/done.sh
+++ jobs/aciss/done.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "$HOSTNAME : Running finalize script."
-cp ${SOS_WORK}/sos_data/* ${SOS_ROOT}/jobs/aciss/data
+echo "$HOSTNAME : Bringing down sosd_probe's..."
+killall sosd_probe
+echo "$HOSTNAME : Moving data files to target directory: ${SOS_ROOT}/jobs/aciss/data/${PBS_JOBID}"
+mkdir ${SOS_ROOT}/jobs/aciss/data/${PBS_JOBID}
+cp -R ${SOS_WORK}/sos_data/* ${SOS_ROOT}/jobs/aciss/data/${PBS_JOBID}
 echo "$HOSTNAME : Done finalizing."
