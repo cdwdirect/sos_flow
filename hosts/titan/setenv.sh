47ecb08e6a83befd510d6c865d85ef37e627649d
Kevin Huck <khuck@cs.uoregon.edu>
2016-12-05 15:25:37 -0500

    minor script changes for titan

--- hosts/titan/setenv.sh
+++ hosts/titan/setenv.sh
@@ -1,3 +1,8 @@
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=mydir="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
+echo $SCRIPTPATH
+BASEDIR=$SCRIPTPATH/../..
+echo $BASEDIR
 
 export SOS_CMD_PORT=22500
 export SOS_DB_PORT=22503
@@ -8,12 +13,16 @@ export CC=gcc
 export CXX=g++
 export MPICC=cc
 export MPICXX=CC
+export TAU_ARCH=craycnl
+export TAU_OPTIONS=-mpi-pthread
+export TAU_ROOT=$HOME/src/tau2
 
 # need to figure out how to use this
-cflags=`cc --cray-print-opts=cflags`
-libs=`cc --cray-print-opts=libs`
-
-extras="-DMPI_C_INCLUDE_PATH=${CRAY_MPICH2_DIR}/include -DMPI_C_LIBRARIES=${CRAY_MPICH2_DIR}/lib/libmpich_gnu_49_mt.so"
+export cflags=`cc --cray-print-opts=cflags`
+export libs=`cc --cray-print-opts=libs`
+export cmake_extras="-DMPI_C_INCLUDE_PATH=${CRAY_MPICH2_DIR}/include -DMPI_C_LIBRARIES=${CRAY_MPICH2_DIR}/lib/libmpich_gnu_49_mt.so"
 
 module load cmake
-module swap PrgEnv-pgi/5.2.82 PrgEnv-gnu
+module swap PrgEnv-pgi PrgEnv-gnu
+
+export sos_env_set=1
