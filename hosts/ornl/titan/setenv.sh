fc4c1e4c6f1616545658d8e177703408549c8ae3
Kevin Huck <khuck@cs.uoregon.edu>
2017-04-03 19:42:13 -0400

    Porting to titan

--- hosts/ornl/titan/setenv.sh
+++ hosts/ornl/titan/setenv.sh
@@ -5,6 +5,9 @@ echo $SCRIPTPATH
 BASEDIR="$(cd "$SCRIPTPATH/../../.."; pwd)"
 echo $BASEDIR
 
+module load cmake
+module swap PrgEnv-pgi PrgEnv-gnu
+
 export SOS_CMD_PORT=22500
 export SOS_DB_PORT=22503
 export SOS_ROOT=$BASEDIR
@@ -19,15 +22,14 @@ export TAU_ARCH=craycnl
 export TAU_CONFIG=-gnu-mpi-pthread
 export TAU_ROOT=/lustre/atlas/proj-shared/csc103/khuck/tau2
 export ADIOS_ROOT=$HOME/src/chaos/adios/ADIOS-gcc
+export CHAOS=$HOME/src/chaos/titan-gcc
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$CHAOS/lib/pkgconfig
 
 # need to figure out how to use this
 export cflags=`cc --cray-print-opts=cflags`
 export libs=`cc --cray-print-opts=libs`
 export cmake_extras="-DMPI_C_INCLUDE_PATH=${CRAY_MPICH2_DIR}/include -DMPI_C_LIBRARIES=${CRAY_MPICH2_DIR}/lib/libmpich_gnu_49.so"
 export cmake_extras_examples="-DMPI_C_INCLUDE_PATH=${CRAY_MPICH2_DIR}/include -DMPI_C_LIBRARIES=${CRAY_MPICH2_DIR}/lib/libmpich_gnu_49.so -DADIOS_ROOT=${ADIOS_ROOT} -DSOS_ROOT=${BASEDIR}/${BUILDDIR}"
-
-module load cmake
-module swap PrgEnv-pgi PrgEnv-gnu
 
 export SOS_ENV_SET=1
 export sos_env_set=1
