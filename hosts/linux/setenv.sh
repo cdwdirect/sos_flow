c32d9d6cc3c930ff9ca9c14e1a08e35e7776a6d7
Kevin Huck <khuck@cs.uoregon.edu>
2017-05-08 10:11:45 -0700

    custom for new hpc cluster

--- hosts/linux/setenv.sh
+++ hosts/linux/setenv.sh
@@ -18,7 +18,11 @@ export TAU_ARCH=x86_64
 export TAU_CONFIG=-mpi-pthread
 export TAU_ROOT=$HOME/src/tau2
 export ADIOS_ROOT=/usr/local/packages/adios/git-gcc-4.9
-export CHAOS=/usr/local/packages/adios/chaos
+if [ `hostname` = "ln1" ] ; then
+	export CHAOS=$HOME/src/chaos/linux-gcc
+else
+	export CHAOS=/usr/local/packages/adios/chaos
+fi
 
 export cmake_extras_examples="-DADIOS_ROOT=${ADIOS_ROOT} -DSOS_ROOT=${BASEDIR}/${BUILDDIR}"
 
@@ -26,6 +30,8 @@ export PKG_CONFIG_PATH=${CHAOS}/lib/pkgconfig:${PKG_CONFIG_PATH}
 export LD_LIBRARY_PATH=${CHAOS}/lib:${ADIOS_ROOT}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CHAOS}/bin:${ADIOS_ROOT}/bin:${PATH}
 
-module load cmake autoconf automake sqlite mpi-tor/openmpi-1.8_gcc-4.9 gcc/4.9 python
+if [ `hostname` != "ln1" ] ; then
+	module load cmake autoconf automake sqlite mpi-tor/openmpi-1.8_gcc-4.9 gcc/4.9 python
+fi
 
 export SOS_ENV_SET=1
