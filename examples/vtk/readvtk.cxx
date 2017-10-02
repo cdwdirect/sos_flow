27949d86794910c349b44bc0df825d1bca778719
Chad Wood <wood67@llnl.gov>
2017-10-02 16:36:58 -0700

    Single-scalar cell coloring works for automatically generated and manually generated lookup tables.

--- examples/vtk/readvtk.cxx
+++ examples/vtk/readvtk.cxx
@@ -4,7 +4,6 @@
 //  https://lorensen.github.io/VTKExamples/site/Cxx/PolyData/ColorCells/
 //
 //
-#include <vtkUnstructuredGridReader.h>
 #include <vtkSmartPointer.h>
 #include <vtkDataSetMapper.h>
 #include <vtkActor.h>
@@ -12,61 +11,114 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
 
+#include <vtkUnstructuredGridReader.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkPointData.h>
+#include <vtkLookupTable.h>
+#include <vtkFloatArray.h>
+#include <vtkCellData.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+
+#include <vtkNamedColors.h>
+
 int main ( int argc, char *argv[] )
 {
-  //parse command line arguments
-  if(argc != 3)
-  {
-    std::cerr << "Usage: " << argv[0]
-              << " FileA(.vtk) FileB(.vtk)" << std::endl;
-    return EXIT_FAILURE;
-  }
+    //parse command line arguments
+    if(argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0]
+            << " FileA(.vtk) FileB(.vtk) colorByIndex" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-  std::string fileA = argv[1];
-  std::string fileB = argv[2];
-
-  //FILE A: read all the data from the file
-  vtkSmartPointer<vtkUnstructuredGridReader> readerA =
-    vtkSmartPointer<vtkUnstructuredGridReader>::New();
-  readerA->SetFileName(fileA.c_str());
-  readerA->Update();
-  //FILE A: Create a mapper and actor
-  vtkSmartPointer<vtkDataSetMapper> mapperA =
-    vtkSmartPointer<vtkDataSetMapper>::New();
-  mapperA->SetInputConnection(readerA->GetOutputPort());
-  vtkSmartPointer<vtkActor> actorA =
-    vtkSmartPointer<vtkActor>::New();
-  actorA->SetMapper(mapperA);
-
-  //FILE B: read all the data from the file
-  vtkSmartPointer<vtkUnstructuredGridReader> readerB =
-    vtkSmartPointer<vtkUnstructuredGridReader>::New();
-  readerB->SetFileName(fileB.c_str());
-  readerB->Update();
-  //FILE B: Create a mapper and actor
-  vtkSmartPointer<vtkDataSetMapper> mapperB =
-    vtkSmartPointer<vtkDataSetMapper>::New();
-  mapperB->SetInputConnection(readerB->GetOutputPort());
-  vtkSmartPointer<vtkActor> actorB =
-    vtkSmartPointer<vtkActor>::New();
-  actorB->SetMapper(mapperB);
+    std::string fileA = argv[1];
+    std::string fileB = argv[2];
+    int colorByIndex  = atoi(argv[3]);
 
 
-  //Create a renderer, render window, and interactor
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->SetSize(600, 300);
+    //FILE A: read all the data from the file
+    vtkSmartPointer<vtkUnstructuredGridReader> readerA =
+        vtkSmartPointer<vtkUnstructuredGridReader>::New();
+    readerA->SetFileName(fileA.c_str());
+    readerA->Update();
+
+    //FILE B: read all the data from the file
+    vtkSmartPointer<vtkUnstructuredGridReader> readerB =
+        vtkSmartPointer<vtkUnstructuredGridReader>::New();
+    readerB->SetFileName(fileB.c_str());
+    readerB->Update();
+
+    //COLOR A: Build a RGB color map automatically.
+    vtkSmartPointer<vtkLookupTable> lutA =
+        vtkSmartPointer<vtkLookupTable>::New();
+    int colorCountA = 512;
+    lutA->SetTableRange(0, colorCountA);
+    lutA->SetHueRange(0.0, 1.0);
+    lutA->SetSaturationRange(0.0, 1.0);
+    lutA->SetAlphaRange(1.0, 1.0);
+    lutA->SetValueRange(0.0, colorCountA);
+    lutA->Build();
+    
+    //COLOR B: Build a color map manually.
+    vtkSmartPointer<vtkLookupTable> lutB =
+        vtkSmartPointer<vtkLookupTable>::New();
+    int colorCountB = 512;
+    lutB->SetNumberOfTableValues(colorCountB);
+    lutB->Build();
+    double rgba[4] = {0.0, 0.0, 1.0, 1.0};
+    lutB->SetTableValue(0, rgba);
+    double colorStep = 1.0 * (1.0 / (double)colorCountB);
+    int i;
+    for (i = 1; i < colorCountB; i++) {
+        rgba[0] = rgba[0] + colorStep;
+        rgba[1] = 0.0;  //rgba[1] - colorStep; 
+        rgba[2] = rgba[2] - colorStep;
+        rgba[3] = 1.0; 
+        lutB->SetTableValue(i, rgba);
+    }
+
+    //FILE A: Create a mapper and actor
+    vtkSmartPointer<vtkDataSetMapper> mapperA =
+        vtkSmartPointer<vtkDataSetMapper>::New();
+
+    mapperA->SetInputConnection(readerA->GetOutputPort());
+    mapperA->SetScalarRange(0, colorCountA - 1);
+    mapperA->SetScalarModeToUseCellData();
+    mapperA->GetInput()->GetCellData()->SetActiveScalars("rank");
+    mapperA->SelectColorArray("rank");
+    mapperA->SetLookupTable(lutA);
+    vtkSmartPointer<vtkActor> actorA =
+        vtkSmartPointer<vtkActor>::New();
+    actorA->SetMapper(mapperA);
+
+    //FILE B: Create a mapper and actor
+    vtkSmartPointer<vtkDataSetMapper> mapperB =
+        vtkSmartPointer<vtkDataSetMapper>::New();
+    mapperB->SetInputConnection(readerB->GetOutputPort());
+    mapperB->SetScalarRange(0, colorCountB - 1);
+    mapperB->SetLookupTable(lutB);
+    //mapperB->SelectColorArray();
+    vtkSmartPointer<vtkActor> actorB =
+        vtkSmartPointer<vtkActor>::New();
+    actorB->SetMapper(mapperB);
+
+
+    //Create a renderer, render window, and interactor
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->SetSize(600, 300);
     // Add one interactor: 
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
-  
-  //Define viewport ranges:
-  double leftViewport[4]  = {0.0, 0.0, 0.5, 1.0};
-  double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
-  
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    //Define viewport ranges:
+    double leftViewport[4]  = {0.0, 0.0, 0.5, 1.0};
+    double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
     //Set up both renderers:
     vtkSmartPointer<vtkRenderer> leftRenderer =
         vtkSmartPointer<vtkRenderer>::New();
@@ -80,16 +132,16 @@ int main ( int argc, char *argv[] )
     rightRenderer->SetViewport(rightViewport);
     rightRenderer->SetBackground(0.4, 0.5, 0.6);
 
-  //Add the actor to the scene
-  leftRenderer->AddActor(actorA);
-  rightRenderer->AddActor(actorB);
+    //Add the actor to the scene
+    leftRenderer->AddActor(actorA);
+    rightRenderer->AddActor(actorB);
 
-  leftRenderer->ResetCamera();
-  rightRenderer->ResetCamera();
+    leftRenderer->ResetCamera();
+    rightRenderer->ResetCamera();
 
-  //Render and interact
-  renderWindow->Render();
-  renderWindowInteractor->Start();
+    //Render and interact
+    renderWindow->Render();
+    renderWindowInteractor->Start();
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
