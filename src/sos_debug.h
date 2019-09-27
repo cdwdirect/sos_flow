70852aa8902f65689b6ff774c5e236245606b13c
Kevin Huck <khuck@cs.uoregon.edu>
2019-09-27 15:26:24 -0700

    disabling debug

--- src/sos_debug.h
+++ src/sos_debug.h
@@ -24,7 +24,7 @@
  *     -1 = disable logging entirely  <--- Faster, but HIDES ALL sosd/sosa logs, too!
  */
 #ifndef SOS_DEBUG
-#define SOS_DEBUG                 0 
+#define SOS_DEBUG                -1 
 #endif
 #define SOS_DEBUG_SHOW_LOCATION   1 
 
